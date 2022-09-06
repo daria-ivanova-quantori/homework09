7dd656d8aaf353ac81db0775174067fe37ec1fca
daria.ivanova <daria.ivanova@quantori.com>
2022-09-06 11:22:01 +0400

    Done with Purchase scenario

--- Behavioral.Automation.Playwright/Behavioral.Automation.Playwright.DemoScenarios/Features/Saucedemo.feature
+++ Behavioral.Automation.Playwright/Behavioral.Automation.Playwright.DemoScenarios/Features/Saucedemo.feature
@@ -1,9 +1,16 @@
 ﻿Feature: Saucedemo
 
-  Scenario: Purchase
-    Given application URL is opened
-    And user entered "standard_user" into "Username"
-    And user entered "secret_sauce" into "Password"
-    When user clicks on "Login button"
-    And user clicks on "Add Backpack to Cart"
-    Then the "Shopping cart badge" text should be "1"
+    Scenario: Purchase
+        Given application URL is opened
+        And user entered "standard_user" into "Username"
+        And user entered "secret_sauce" into "Password"
+        When user clicks on "Login button"
+        And user clicks on "Add Backpack to Cart"
+        And user clicks on "Cart Button"
+        And user clicks on "Checkout Button"
+        And user enters "firstName" into "First Name"
+        And user enters "lastName" into "Last Name"
+        And user enters "0160" into "Postal Code"
+        And user clicks on "Continue Button"
+        And user clicks on "Finish Button"
+        Then the "Page Header" text should be "Checkout: Complete!"
--- Behavioral.Automation.Playwright/Behavioral.Automation.Playwright/Pages/MainPage.cs
+++ Behavioral.Automation.Playwright/Behavioral.Automation.Playwright/Pages/MainPage.cs
@@ -7,15 +7,26 @@ namespace Behavioral.Automation.Playwright.Pages;
 class MainPage : ISelectorStorage
 {
     private static readonly string Id = ConfigManager.GetConfig<Config>().SearchAttribute;
+
+    public ElementSelector PageHeader = new() { Selector = "//span[@class='title']" };
     
     //Login
-    public ElementSelector Username = new() { IdSelector = "username"};
-    public ElementSelector Password = new() { IdSelector = "password"};
-    public ElementSelector LoginButton = new() { IdSelector = "login-button"};
-    
+    public ElementSelector Username = new() { IdSelector = "username" };
+    public ElementSelector Password = new() { IdSelector = "password" };
+    public ElementSelector LoginButton = new() { IdSelector = "login-button" };
+
     //Items
     public ElementSelector AddBackpackToCart = new() { IdSelector = "add-to-cart-sauce-labs-backpack" };
-    
+
     //ShoppingCart
-    public ElementSelector ShoppingCartBadge = new() { Selector = "//span[@class='shopping_cart_badge']"};
+    public ElementSelector ShoppingCartBadge = new() { Selector = "//span[@class='shopping_cart_badge']" };
+    public ElementSelector CartButton = new() { Selector = "//div[@class='shopping_cart_container']" };
+    public ElementSelector CheckoutButton = new() { IdSelector = "checkout" };
+
+    //Checkout 
+    public ElementSelector FirstName = new() { IdSelector = "firstName" };
+    public ElementSelector LastName = new() { IdSelector = "lastName" };
+    public ElementSelector PostalCode = new() { IdSelector = "postalCode" };
+    public ElementSelector ContinueButton = new() { IdSelector = "continue" };
+    public ElementSelector FinishButton = new() { IdSelector = "finish" };
 }
